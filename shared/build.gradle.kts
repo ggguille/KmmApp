21f366f6b50712f593edcbe9998c9b9e117665fb
Guille <g.granado.g@gmail.com>
2021-04-11 15:14:21 +0200

    Configuring sql delight (#3)
    * config slqdelight database
    
    * ignore build folders
    
    * remove build folders from tracking
    
    * fix duplicate folder
    
    * generate sql commands
    
    * reconfigura sqldelight and fix ios configuration
    
    * declare and implement DatabseDriverFactory
    
    * implement database

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,14 +9,21 @@ plugins {
 }
 
 kotlin {
-    android()
-    ios {
+    val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
+        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
+            ::iosArm64
+        else
+            ::iosX64
+
+    iOSTarget("ios") {
         binaries {
             framework {
                 baseName = "shared"
             }
         }
     }
+
+    android()
 
     val ktorVersion = "1.4.0"
     val serializationVersion = "1.0.0-RC"
@@ -71,12 +78,16 @@ android {
     }
 }
 
+sqldelight {
+    database("AppDatabase") {
+        packageName = "com.example.kmmapp.shared.cache"
+    }
+}
+
 val packForXcode by tasks.creating(Sync::class) {
     group = "build"
     val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
-    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
-    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+    val framework = kotlin.targets.getByName<KotlinNativeTarget>("ios").binaries.getFramework(mode)
     inputs.property("mode", mode)
     dependsOn(framework.linkTask)
     val targetDir = File(buildDir, "xcode-frameworks")
